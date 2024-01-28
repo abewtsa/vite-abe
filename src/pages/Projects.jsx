ac0515fc9ee6020663b6114413fa65b6ee9a515c
abewtsa <105269475+abewtsa@users.noreply.github.com>
2024-01-28 21:34:00 +1100

    remove netlink

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -20,19 +20,8 @@ const Projects = () => {
     />
     <p>
     <a className="special-link" target="_blank" href="https://www.figma.com/proto/tJlMYdV8eiVzqFkMnDXEZG/YESFRESH?page-id=0%3A1&type=design&node-id=1050-12583&viewport=1268%2C279%2C0.17&t=MirwrGpoXWsJLQ8K-1&scaling=min-zoom&starting-point-node-id=1050%3A12583&mode=design">
-    <img width='450px' height='300px' src='https://raw.githubusercontent.com/abewtsa/vite-abe/master/public/YesFresh-Splash.jpeg'></img></a><br /><br />
-    
-    Netlink - an early prototype pitched for a static agency site.</p>
-
-    <Button
-        buttonClassName='btn'
-        buttonText='See in Figma'
-        buttonFunc=''
-        buttonLink='https://www.figma.com/proto/O6vsMOXDTZaAfZeLw7wu9J/Netlink-mockup-14-Dec-2023?page-id=0%3A1&type=design&node-id=98-533&viewport=1070%2C2777%2C0.26&t=NolAobrZsJ0sYSA1-1&scaling=min-zoom&starting-point-node-id=98%3A533&mode=design'
-    />
-
-    <p>
-    <a className="special-link" target="_blank" href="https://www.figma.com/proto/O6vsMOXDTZaAfZeLw7wu9J/Netlink-mockup-14-Dec-2023?page-id=0%3A1&type=design&node-id=98-533&viewport=1070%2C2777%2C0.26&t=NolAobrZsJ0sYSA1-1&scaling=min-zoom&starting-point-node-id=98%3A533&mode=design"><img width='450px' height='300px' src='https://raw.githubusercontent.com/abewtsa/vite-abe/master/public/Netlink-home.jpg'></img></a></p>
+    <img width='450px' height='300px' src='https://raw.githubusercontent.com/abewtsa/vite-abe/master/public/YesFresh-Splash.jpeg'></img></a>
+    </p>
 
     <p>Monezi - financial advice and ethical micro loan financial product app.</p>
     <Button
@@ -42,7 +31,9 @@ const Projects = () => {
         // buttonLink=''
     />
     <p>
-    <a className="special-link" target="_blank" href="https://www.figma.com/proto/O6vsMOXDTZaAfZeLw7wu9J/Netlink-mockup-14-Dec-2023?page-id=0%3A1&type=design&node-id=98-533&viewport=1070%2C2777%2C0.26&t=NolAobrZsJ0sYSA1-1&scaling=min-zoom&starting-point-node-id=98%3A533&mode=design"><img width='450px' height='300px' src='https://raw.githubusercontent.com/abewtsa/vite-abe/master/public/Monezi-mobile.png'></img></a></p>
+    <img width='450px' height='300px' src='https://raw.githubusercontent.com/abewtsa/vite-abe/master/public/Monezi-mobile.png'>
+    </img>
+    </p>
 
 
     </Section>
