b27ece2e7a7a54fe61b78e4401194c60477d1b07
Abraham Wongso <105269475+abewtsa@users.noreply.github.com>
2024-04-11 10:15:55 +1000

    Home quick update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,12 +10,11 @@ return (
     <div className='container-680'>
 
       <Section
-      header="Hello and welcome 👋"
-      textcopy="I'm Abraham and this my portfolio site where you can find my projects, UX thoughts and Figma designs.
+      header="Abraham Wongso - UX / UI / Front End"
+      textcopy="Thanks for visiting this site, I'm UX/UI Designer with great interest in Front End technologies and delivering great digital experiences.
 
-      I've been working in UI/UX for the past 5 years and currently having a lot of fun delving into front end stuffs. The projects and designs revolve around Figma, React and NextJS. This site can be a bit messy (and will be updated a lot), do please check things out. It will be migrated – or rebuilt from scratch – to Astro or NextJS at some point.
-
-      Curious on my thoughts on UX/UI and Front End? Check out the About blog section.
+      For the past 5 years, my UI/UX work has been largely in enterprise and macro UX/CX, with hands-on involvement in content update, management and micro UX design and advisory for one of the large Australian NFP.
+      My projects and designs that you can explore in this site revolve around Figma, React and NextJS. While this site is still a work in progress, I'm hopeful that you will find useful information.
 
       Figma links and projects are listed below, and you can reach out to me by email."
       >
