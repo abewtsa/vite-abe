81b80bdaba338008d0b0fa83b97337616ba41abb
Abe W <105269475+abewtsa@users.noreply.github.com>
2024-06-17 19:10:02 +1000

    updated home hero image paths

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -11,11 +11,7 @@ const HeroCard = () => {
       <div target="_blank" rel="noreferrer" className="hero-bio-container">
         <a href="mailto:abrams.wong@gmail.com">
           <div className="hero-bio-block">
-            <img
-              className="hero-bio-icon"
-              alt=""
-              src="https://raw.githubusercontent.com/abewtsa/vite-abe/a8ee4dd140f4f5915ddfedcf9abd7cf506e46360/public/email.svg"
-            />
+            <img className="hero-bio-icon" alt="" src="/email.svg" />
             <span className="hero-span">Email</span>
           </div>
         </a>
@@ -26,26 +22,14 @@ const HeroCard = () => {
           href="https://www.linkedin.com/in/abraham-wongso-50795152/"
         >
           <div className="hero-bio-block">
-            <img
-              className="hero-bio-icon"
-              alt=""
-              src="https://raw.githubusercontent.com/abewtsa/vite-abe/a8ee4dd140f4f5915ddfedcf9abd7cf506e46360/public/linkedin.svg"
-            />
+            <img className="hero-bio-icon" alt="" src="/linkedin.svg" />
             <span className="hero-span">Linkedin</span>
           </div>
         </a>
 
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="public/Abraham-Resume-May30.pdf"
-        >
+        <a target="_blank" rel="noreferrer" href="/Abraham-Resume-May30.pdf">
           <div className="hero-bio-block">
-            <img
-              className="hero-bio-icon"
-              alt=""
-              src="https://raw.githubusercontent.com/abewtsa/vite-abe/a8ee4dd140f4f5915ddfedcf9abd7cf506e46360/public/opencv.svg"
-            />
+            <img className="hero-bio-icon" alt="" src="/opencv.svg" />
             <span className="hero-span">Open CV</span>
           </div>
         </a>
